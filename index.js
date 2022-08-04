c0f04ab5969c5f0f115d906fa3af284a8a92b7d9
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-04 13:28:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function introduction(name){
-    console.log(`Hi, my name is ${name}.`)
+    return(`Hi, my name is ${name}.`)
 }
 function introductionWithLanguage(name,language){
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
-function introductionWithLanguageOptional(name,language="Javasript"){
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`)
-}
-function introductionWithLanguageOptional(name,language="Javasript"){
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+
+function introductionWithLanguageOptional(name,language="JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 introduction("Aki");
 introductionWithLanguage("Aki","Ember.js");
 introductionWithLanguageOptional("Gracie");
-introductionWithLanguageOptional("Gracie","Python");
+introductionWithLanguage("Gracie","Python");
